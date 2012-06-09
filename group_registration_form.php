982f85f8a27c94b4e0a03bfcdbdc78e549285342
Robert Wall <robert@rww.name>
2012-06-09 12:17:57 -0700

    adding grf-f.php and group_registration_form.php, which document the suspension of group registration

---
+++ group_registration_form.php
@@ -0,0 +1,15 @@
+<!--#set var="more_meta" value="" -->
+<!--#set var="page_title" value="freenode: Group Registration Form" -->
+<!--#set var="content_title" value="Group Registration Form" -->
+<!--#include file="include/header-mainlogos.shtml" -->
+
+<p><span class="freenode">freenode</span> group registration has been
+temporarily suspended. For more information, please visit <a
+href="http://blog.freenode.net/?p=1609">the announcement on freenode
+staffblog</a>. If you have questions not covered by that announcement, please <a
+href="http://freenode.net/faq.shtml#helpfromstaff">contact a staffer</a>.</p>
+
+<!--#set var="SPONSOR_LINKS" value="<small>
+        </small>"-->
+
+<!--#include file="include/trailer.shtml" -->
